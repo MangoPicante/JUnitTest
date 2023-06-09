02e41fb4e48fb9d9b1717722d300c1f36aa02f67
Sean Michael <8892151+MangoPicante@users.noreply.github.com>
2023-06-09 16:33:00 -0700

    implemented detectCapitalUse method

--- src/CILab.java
+++ src/CILab.java
@@ -1,3 +1,6 @@
+import java.util.Locale;
+import java.util.StringTokenizer;
+
 public class CILab implements CILabInterface {
     private String myString;
 
@@ -13,7 +16,26 @@ public class CILab implements CILabInterface {
 
     @Override
     public boolean detectCapitalUse() {
-        return false;
+        // blank string returns true
+        if (myString.isBlank()) return true;
+        // anything but letters returns false
+        if (!myString.matches("[a-zA-Z]+")) return false;
+        // set up to test remaining cases
+        // records if first char is capital
+        boolean firstCharCap = Character.isUpperCase(myString.charAt(0));
+        // tracks if there are only upper or lower chars
+        boolean allLower = true;
+        boolean allUpper = true;
+        for (char c: myString.substring(1).toCharArray()) {
+            if (Character.isUpperCase(c)) allLower = false;
+            if (Character.isLowerCase(c)) allUpper = false;
+            if (!allUpper && !allLower) return false;
+        }
+        // if the first char is capital and the remainder are homogeneous, return true
+        if (firstCharCap) return true;
+        // if the first char is not capital and the remainder are lower, return true
+        return allLower;
+
     }
 
 }
